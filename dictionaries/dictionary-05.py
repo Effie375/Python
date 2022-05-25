7677f7fe5054f3adc65ac354067b985d2aeb6011
Efstratia Kazi <efoulakaz@hotmail.com>
2022-05-25 09:54:15 +0200

    Update dictionary-05.py

--- dictionaries/dictionary-05.py
+++ dictionaries/dictionary-05.py
@@ -1,8 +1,3 @@
-thisdict = {
-  "brand": "Ford",
-  "electric": False,
-  "year": 1964,
-  "colors": ["red", "white", "blue"]
-}
+thisdict = {"brand": "Ford", "electric": False, "year": 1964, "colors": ["red", "white", "blue"]}
 
 print(thisdict)
