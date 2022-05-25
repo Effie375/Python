0bfafc908f318d0d8c38a0cc43d6fc21a91ca0ea
Efstratia Kazi <efoulakaz@hotmail.com>
2022-05-25 10:09:20 +0200

    Update dictionary-copy-02.py

--- dictionaries/dictionary-copy-02.py
+++ dictionaries/dictionary-copy-02.py
@@ -1,11 +1,5 @@
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
 
 mydict = thisdict
-
 thisdict["year"] = 2000
-
 print(mydict)
