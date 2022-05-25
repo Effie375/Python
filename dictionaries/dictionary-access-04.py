fefd7fe80ccc68a6585eb58221b0e31e903ca71e
Efstratia Kazi <efoulakaz@hotmail.com>
2022-05-25 09:57:05 +0200

    Update dictionary-access-04.py

--- dictionaries/dictionary-access-04.py
+++ dictionaries/dictionary-access-04.py
@@ -1,13 +1,7 @@
-car = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
+car = {"brand": "Ford", "model": "Mustang", "year": 1964}
 
 x = car.keys()
-
 print(x) #before the change
 
 car["color"] = "white"
-
 print(x) #after the change
