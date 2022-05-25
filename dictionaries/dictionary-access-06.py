809558248d01135c26703cae171591c09ef96338
Efstratia Kazi <efoulakaz@hotmail.com>
2022-05-25 09:58:15 +0200

    Update dictionary-access-06.py

--- dictionaries/dictionary-access-06.py
+++ dictionaries/dictionary-access-06.py
@@ -1,13 +1,7 @@
-car = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
+car = {"brand": "Ford", "model": "Mustang", "year": 1964}
 
 x = car.values()
-
 print(x) #before the change
 
 car["year"] = 2020
-
 print(x) #after the change
