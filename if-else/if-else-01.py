409dd04a3014b6755f6fe11580a06ee6836e572b
Efstratia Kazi <efoulakaz@hotmail.com>
2022-05-25 10:29:11 +0200

    Update if-else-01.py

--- if-else/if-else-01.py
+++ if-else/if-else-01.py
@@ -1,8 +1,9 @@
 a = 200
 b = 33
+
 if b > a:
-  print("b is greater than a")
+    print("b is greater than a")
 elif a == b:
-  print("a and b are equal")
+    print("a and b are equal")
 else:
-  print("a is greater than b")
+    print("a is greater than b")
