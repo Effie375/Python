298cf4592682d7b12ae1a41a05d10c8afdb1a6b4
Efstratia Kazi <efoulakaz@hotmail.com>
2022-05-25 10:20:31 +0200

    Update dictionary-loop-03.py

--- dictionaries/dictionary-loop-03.py
+++ dictionaries/dictionary-loop-03.py
@@ -1,8 +1,4 @@
-thisdict =	{
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
 
 for x in thisdict.values():
-  print(x)
+    print(x)
