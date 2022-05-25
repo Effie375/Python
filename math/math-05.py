7d78c14f3d3d4e4192fbda2076b5458a10df5531
Efstratia Kazi <efoulakaz@hotmail.com>
2022-05-25 09:46:56 +0200

    Update math-05.py

--- math/math-05.py
+++ math/math-05.py
@@ -4,10 +4,9 @@ import math
 #Round a number upward to its nearest integer
 #x = math.ceil(-1.4)
 x = math.ceil(1.4)
+print(x)
 
 #Round a number downward to its nearest integer
 #y = math.floor(-1.9)
 y = math.floor(1.9)
-
-print(x)
 print(y)
