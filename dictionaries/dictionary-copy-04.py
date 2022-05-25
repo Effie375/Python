0f84ca421013d9fd8406eb01ab9bb1fac174d4e9
Efstratia Kazi <efoulakaz@hotmail.com>
2022-05-25 10:10:26 +0200

    Update dictionary-copy-04.py

--- dictionaries/dictionary-copy-04.py
+++ dictionaries/dictionary-copy-04.py
@@ -1,14 +1,8 @@
 from copy import deepcopy
 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "years": [1964, 1983, 2000]
-}
+thisdict = {"brand": "Ford", "model": "Mustang", "years": [1964, 1983, 2000]}
 
 # Deep copy
 mydict = deepcopy(thisdict)
-
 thisdict["years"][2] = 2022
-
 print(mydict)
