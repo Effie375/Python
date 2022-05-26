6818414e0972f99399c268b6d65ead87b0e8d1a5
Efstratia Kazi <efoulakaz@hotmail.com>
2022-05-26 16:48:47 +0200

    Update string-formatting-03.py

--- string-formatting/string-formatting-03.py
+++ string-formatting/string-formatting-03.py
@@ -1,5 +1,5 @@
 first = "Ευστρατία"
 last = "Καζή"
 height = 1.65
-myorder = "Ονομάζομαι {} {} και είμαι {:.2f}."
-print(myorder.format(first, last, height))
+txt = "Ονομάζομαι {} {} και είμαι {:.2f}."
+print(txt.format(first, last, height))
