b051f3da619fb08f50ec367ce90ec2d7e6353c7e
Efstratia Kazi <efoulakaz@hotmail.com>
2022-05-26 16:45:27 +0200

    Update string-formatting-04.py

--- string-formatting/string-formatting-04.py
+++ string-formatting/string-formatting-04.py
@@ -1,5 +1,5 @@
-quantity = 3
-itemno = 567
-price = 49
-myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
-print(myorder.format(quantity, itemno, price))
+first = "Ευστρατία"
+last = "Καζή"
+height = 1.65
+myorder = "Ονομάζομαι {0} {1} και είμαι {2:.2f}."
+print(myorder.format(first, last, height))
