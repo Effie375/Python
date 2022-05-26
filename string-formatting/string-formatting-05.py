3f0cfdbc83d16b0df598033ebc4ceeec87da5866
Efstratia Kazi <efoulakaz@hotmail.com>
2022-05-26 16:49:14 +0200

    Update string-formatting-05.py

--- string-formatting/string-formatting-05.py
+++ string-formatting/string-formatting-05.py
@@ -1,5 +1,5 @@
 first = "Ευστρατία"
 last = "Καζή"
 height = 1.65
-myorder = "Το όνομα της είναι {0} {1}. Η {0} είναι {2:.2f}."
-print(myorder.format(first, last, height))
+txt = "Το όνομα της είναι {0} {1}. Η {0} είναι {2:.2f}."
+print(txt.format(first, last, height))
