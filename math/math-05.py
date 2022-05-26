60986782cc004af9e28bd71e61c5ef8236d2d6e0
Efstratia Kazi <efoulakaz@hotmail.com>
2022-05-26 16:34:53 +0200

    Update math-05.py

--- math/math-05.py
+++ math/math-05.py
@@ -1,9 +1,10 @@
 import math
 
-#x = math.ceil(-1.4)
-x = math.ceil(1.4)
-print(x)
-
-#y = math.floor(-1.9)
-y = math.floor(1.9)
-print(y)
+a = math.ceil(-1.4)
+print(a)
+b = math.ceil(1.4)
+print(b)
+c = math.floor(-1.9)
+print(c)
+d = math.floor(1.9)
+print(d)
