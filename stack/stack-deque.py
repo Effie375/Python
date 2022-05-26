178174e02b65b500180096032eb44619e034aba1
Efstratia Kazi <efoulakaz@hotmail.com>
2022-05-26 21:36:39 +0300

    Add files via upload

---
+++ stack/stack-deque.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+myStack = deque()
+
+print(myStack)
+
+myStack.append('a')
+myStack.append('b')
+myStack.append('c')
+
+print(myStack)
+
+myStack.pop()
+print(myStack)
+myStack.pop()
+print(myStack)
+myStack.pop()
+print(myStack)
---
+++ stack/stack-list.py
@@ -0,0 +1,16 @@
+myStack = []
+
+print(myStack)
+
+myStack.append('a')
+myStack.append('b')
+myStack.append('c')
+
+print(myStack)
+
+myStack.pop()
+print(myStack)
+myStack.pop()
+print(myStack)
+myStack.pop()
+print(myStack)
