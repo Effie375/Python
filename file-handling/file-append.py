bef93be6cf3c7b8e0eadefd5badfd73da0742669
Efstratia Kazi <efoulakaz@hotmail.com>
2022-06-01 11:13:29 +0200

    Update file-append.py

--- file-handling/file-append.py
+++ file-handling/file-append.py
@@ -1,7 +1,6 @@
 f = open("demofile.txt", "a")
-f.write("Now the file has more content!")
+f.write("Καζή Ευστρατία")
 f.close()
 
-#open and read the file after the appending:
 f = open("demofile.txt", "r")
 print(f.read())
