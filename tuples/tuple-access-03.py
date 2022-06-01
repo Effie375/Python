4e430967a81a5adc4f15e139c1224d3839262bb3
Efstratia Kazi <efoulakaz@hotmail.com>
2022-06-01 09:06:41 +0200

    Update tuple-access-03.py

--- tuples/tuple-access-03.py
+++ tuples/tuple-access-03.py
@@ -1,7 +1,2 @@
-thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
-print(thistuple[2:5])
-
-#This will return the items from position 2 to 5.
-
-#Remember that the first item is position 0,
-#and note that the item in position 5 is NOT included
+mytuple = ("Μήλο", "Μπανάνα", "Κεράσι", "Ανανάς", "Καρπούζι", "Φράουλα", "Αχλάδι")
+print(mytuple[2:5])
